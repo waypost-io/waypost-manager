fad38bc6a39815931e3048e3a75076f928bbadc4
Julia Martin <juliadmartin720@gmail.com>
2022-03-01 14:42:10 -0800

    Create main components for frontend

---
+++ client/src/components/FlagItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FlagItem = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default FlagItem;
---
+++ client/src/components/FlagsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FlagItem from './FlagItem';
+
+const FlagsList = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default FlagsList;
