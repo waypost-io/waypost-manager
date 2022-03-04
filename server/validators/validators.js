dadb16e0e80c0a0c5eedbecd255b2b7b0c3aea8e
sean <sean.richardson.umd18@gmail.com>
2022-03-03 17:23:16 -0800

    Added edit & delete functionality

--- server/validators/validators.js
+++ server/validators/validators.js
@@ -1,5 +1,7 @@
 const { check, oneOf } = require("express-validator");
+const { FLAGS_COL_NAMES } = require("../constants/db");
 
 exports.validateNewFlag = [check("name").not().isEmpty()];
-exports.validateEditFlag = [];
+// exports.validateEditFlag = [body(check("*").oneOf.FLAGS_COL_NAMES];
+// maybe do some schema validation https://express-validator.github.io/docs/schema-validation.html
 exports.validateSDKKey = [];
