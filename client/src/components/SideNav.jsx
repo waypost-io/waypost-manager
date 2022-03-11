5d877300cafcd405fa9355452c4fe1f406c510ae
Julia Martin <juliadmartin720@gmail.com>
2022-03-10 17:05:33 -0800

    Convert SideNav's CSS to Tailwind classes

--- client/src/components/SideNav.jsx
+++ client/src/components/SideNav.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
+const navLinks =[
+  { title: 'Flags Dashboard', path: "/" },
+  { title: 'Event Log', path: "/"},
+  { title: 'Get an SDK Key', path: "/"}
+]
+
 const SideNav = () => {
   return (
-    <div className="bg-primary-black">
+    <div className="bg-primary-black text-primary-offwhite min-h-screen w-80 min-w-min">
       <img src="assets/PNGs/Waypost_logo_on_dark.png" alt="Waypost" />
-      <ul className="sidenav-list">
-        <a href="/"><li>Flags Dashboard</li></a>
-        <li>Event Log</li>
-        <li>Tech Debt Dashboard</li>
-        <li>Get a new SDK Key</li>
+      <ul className="font-bold p-0">
+        {navLinks.map(item => {
+          return (
+            <a href={item.path}>
+              <li className="p-5 hover:bg-primary-violet hover:text-primary-offwhite">{item.title}</li>
+            </a>
+            )
+        })}
       </ul>
     </div>
   );
