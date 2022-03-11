46a4b8fbd1aad21ecb56b2b21d03756e041cd3b6
Julia Martin <juliadmartin720@gmail.com>
2022-03-10 16:33:47 -0800

    Install Tailwind and set up config with color palettes, fix logo size

---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: {
+        'black': '#20232B',
+        'oxford_blue': '#0C182E',
+        'offwhite': '#F5F8FC',
+        'violet': '#B94E9C',
+        'turquoise': '#07C0C5'
+      },
+      secondary: {
+        'green': '#8FE3A5',
+        'pink': '#FCB8EA',
+        'salmon': '#F19693',
+        'skyblue': '#B0FAFC'
+      }
+    },
+    fontFamily: {
+      sans: ['Montserrat Regular', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
