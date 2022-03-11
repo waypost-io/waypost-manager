5f8d501e0ab3a0e06545512df22e2887317d15eb
Julia Martin <juliadmartin720@gmail.com>
2022-03-10 18:06:59 -0800

    Add link to homepage on header title

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="w-full p-4 bg-primary-black border-b-2 border-b-primary-violet text-primary-offwhite font-display text-xl">
-      <h1>Waypost</h1>
+      <a href="/" className="hover:text-secondary-skyblue"><h1>Waypost</h1></a>
     </header>
   );
 };
