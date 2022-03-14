2a2c5a900356d1e7c82ec76b025920fb921a7c80
Julia Martin <juliadmartin720@gmail.com>
2022-03-13 17:56:54 -0700

    Delete console log statement

--- client/src/reducers/experiments.js
+++ client/src/reducers/experiments.js
@@ -1,7 +1,6 @@
 export default function experiments(state = [], action) {
   switch (action.type) {
     case "FETCH_EXPERIMENTS_SUCCESS": {
-      console.log(action.experiments);
       return action.experiments;
     }
     default: {
