60653e8b353fefa12cbf7cdc6bd90d2cf04ac431
Julia Martin <juliadmartin720@gmail.com>
2022-03-15 15:32:18 -0700

    Add header to metrics page

--- client/src/components/MetricsPage.jsx
+++ client/src/components/MetricsPage.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const MetricsPage = () => {
   return (
-    <div>
-
+    <div className="w-full py-2.5 px-12">
+      <div className="flex justify-between items-center p-5">
+        <h2 className="text-xl font-bold">Metrics</h2>
+        <button
+          className="btn bg-primary-turquoise"
+          type="button"
+          onClick={() => {}}
+        >
+          Create New
+        </button>
+      </div>
     </div>
   );
 };
