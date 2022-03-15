16c94424419140a6a061c4fe959c8c3f3eb03f4b
Julia Martin <juliadmartin720@gmail.com>
2022-03-15 16:20:39 -0700

    Change border color

--- client/src/components/MetricItem.jsx
+++ client/src/components/MetricItem.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const MetricsItem = ({ id, name, query_string, type }) => {
   return (
-    <div className="border border-slate my-5 p-5">
-      <p className="text-lg font-bold">{name}</p>
-      <p>Type: {type}</p>
+    <div className="flex justify-between items-center border border-primary-black rounded my-5 p-5">
+      <div>
+        <p className="text-lg font-bold">{name}</p>
+        <p>Type: {type}</p>
+      </div>
+      <div>
+        <button className="bg-primary-violet text-primary-offwhite rounded py-2 px-5 block mb-2">Edit</button>
+        <button className="hover:text-primary-violet">Delete</button>
+      </div>
     </div>
   );
 }
