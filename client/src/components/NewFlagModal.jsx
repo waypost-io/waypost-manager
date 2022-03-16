a5db0d507d4505816c0fe2fa9a59b7555bd8eb3d
Julia Martin <juliadmartin720@gmail.com>
2022-03-16 15:44:13 -0700

    Improve look of New Flag modal

--- client/src/components/NewFlagModal.jsx
+++ client/src/components/NewFlagModal.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { createFlag } from '../actions/flagActions';
+import { createFlag } from "../actions/flagActions";
 
 const NewFlagModal = ({ modalOpen, setModalOpen }) => {
   const dispatch = useDispatch();
-  const flags = useSelector(state => state.flags);
-  const flagNames = flags.map(flag => flag.name);
-  const [ name, setName ] = useState('');
-  const [ description, setDescription ] = useState('');
-  const [ status, setStatus ] = useState(false);
-  const [ percentage, setPercentage ] = useState(100);
+  const flags = useSelector((state) => state.flags);
+  const flagNames = flags.map((flag) => flag.name);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [status, setStatus] = useState(false);
+  const [percentage, setPercentage] = useState(100);
 
   const handleCloseModal = () => {
     setModalOpen(false);
@@ -17,11 +17,11 @@ const NewFlagModal = ({ modalOpen, setModalOpen }) => {
 
   const resetForm = () => {
     setModalOpen(false);
-    setName('');
-    setDescription('');
+    setName("");
+    setDescription("");
     setStatus(false);
     setPercentage(100);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -38,26 +38,81 @@ const NewFlagModal = ({ modalOpen, setModalOpen }) => {
     <div className={`overlay ${modalOpen ? "" : "hidden"}`}>
       <div className="new-flag-modal">
         <i className="x-icon icon close-modal" onClick={handleCloseModal}></i>
-        <h2 className="font-bold text-xl text-primary-violet">New Feature Flag</h2>
+        <h2 className="font-bold text-xl text-primary-violet mt-5">
+          New Feature Flag
+        </h2>
         <form className="new-flag-form">
-          <div className="mt-2.5">
-            <label htmlFor="flag-title" className="mr-5">Name (will be used as unique identifier):</label>
-            <input id="flag-title" type="text" className="border border-primary-oxfordblue rounded-lg px-2" value={name} onChange={(e) => setName(e.target.value)} />
+          <div className="mt-2.5 flex items-center">
+            <label
+              htmlFor="flag-title"
+              className="inline-block flex-1 text-right mr-5"
+            >
+              Name (will be used as unique identifier):
+            </label>
+            <input
+              id="flag-title"
+              type="text"
+              className="border flex-1 border-slate rounded-lg p-2"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
-          <div className="mt-2.5">
-            <label htmlFor="flag-description" className="mr-5">Description:</label>
-            <input id="flag-description" type="text" className="border border-primary-oxfordblue rounded-lg px-2" value={description} onChange={(e) => setDescription(e.target.value)} />
+          <div className="mt-2.5 flex items-center">
+            <label
+              htmlFor="flag-description"
+              className="inline-block flex-1 text-right mr-5"
+            >
+              Description:
+            </label>
+            <input
+              id="flag-description"
+              type="text"
+              className="border border-slate flex-1 rounded-lg p-2"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
           </div>
-          <div className="mt-2.5">
-            <label htmlFor="flag-active" className="mr-5">Active?</label>
-            <input id="flag-active" type="checkbox" checked={status} onChange={(e) => setStatus(e.target.checked)} />
+          <div className="mt-2.5 flex items-center">
+            <label
+              htmlFor="flag-active"
+              className="inline-block w-1/2 text-right mr-5"
+            >
+              Active?
+            </label>
+            <input
+              id="flag-active"
+              type="checkbox"
+              checked={status}
+              onChange={(e) => setStatus(e.target.checked)}
+            />
           </div>
-          <div className="mt-2.5">
-            <label htmlFor="flag-percent" className="mr-5">Percent of Users Exposed</label>
-            <input id="flag-percent" type="number" max={100} min={0} size="3" className="border border-primary-oxfordblue rounded-lg px-2" value={percentage} onChange={(e) => setPercentage(e.target.value)} /> %
+          <div className="mt-2.5 flex items-center">
+            <label
+              htmlFor="flag-percent"
+              className="inline-block w-1/2 text-right mr-5"
+            >
+              Percent of Users Exposed
+            </label>
+            <input
+              id="flag-percent"
+              type="number"
+              max={100}
+              min={0}
+              size="3"
+              className="border border-slate flex-0 rounded-lg p-2"
+              value={percentage}
+              onChange={(e) => setPercentage(e.target.value)}
+            />
+            <span className="ml-2">%</span>
           </div>
-          <button type="submit" className="btn bg-primary-violet" onClick={handleSubmit}>Submit</button>
         </form>
+        <button
+          type="submit"
+          className="btn bg-primary-violet"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
