aef7f1d4efa9fd171175d7133460669fe1e35a1e
Julia Martin <juliadmartin720@gmail.com>
2022-03-18 12:47:58 -0700

    Add both groups to exposures chart

--- client/src/components/ExposuresChart.jsx
+++ client/src/components/ExposuresChart.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import Chart from "react-apexcharts";
 
 const ExposuresChart = ({ id }) => {
-  const series = [{ name: "exposures", data: [5, 10, 20, 35, 50, 70] }];
+  const series = [
+    { name: "control", data: [5, 10, 20, 35, 50, 70] },
+    { name: "test", data: [6, 9, 19, 36, 51, 69] }
+  ];
   const chartOptions = {
     chart: { id },
     xaxis: {
       categories: ['2022-03-09', '2022-03-10', '2022-03-11', '2022-03-12', '2022-03-13', '2022-03-14']
     },
-    title: { text: "Exposures to Experiment Over Time" },
-    colors: ['#07C0C5']
+    title: { text: "Total Exposures to Experiment (Sample Size)" },
+    colors: ['#07C0C5', '#B94E9C'],
+    stroke: {
+      width: 1.5
+    }
   };
 
   return (
@@ -18,7 +24,7 @@ const ExposuresChart = ({ id }) => {
         options={chartOptions}
         series={series}
         type="line"
-        width="400"
+        width="450"
       />
     </div>
   );
