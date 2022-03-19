97515ff6102fe2848d8b643daa78622fc1fefed9
Julia Martin <juliadmartin720@gmail.com>
2022-03-18 19:50:29 -0700

    More styling

--- client/src/components/ExperimentResults.jsx
+++ client/src/components/ExperimentResults.jsx
@@ -20,8 +20,8 @@ const ExperimentResults = ({ id }) => {
         <button onClick={handleRefreshResults} className="btn bg-primary-violet">Refresh Results</button>
       </div>
       {hasResult && (
-        <table className="w-full border border-primary-slate">
-          <thead>
+        <table className="w-full shadow-md">
+          <thead className="bg-primary-black text-primary-offwhite">
             <tr>
               <th>Metric</th>
               <th>Control Group Avg</th>
