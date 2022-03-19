1dadf87a6b69b9f3be67939410d5513df678656b
Julia Martin <juliadmartin720@gmail.com>
2022-03-18 19:30:26 -0700

    Add interval visualization and format numbers

--- client/src/components/ExperimentResults.jsx
+++ client/src/components/ExperimentResults.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Chart from "react-apexcharts";
+import React from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { updateStats } from '../actions/exptActions';
+import MetricResultRow from './MetricResultRow';
 
 const ExperimentResults = ({ id }) => {
   const metrics = useSelector((state) => state.experiments.find(expt => expt.id === id).metrics);
@@ -31,25 +31,7 @@ const ExperimentResults = ({ id }) => {
             </tr>
           </thead>
           <tbody >
-            {metrics.map(metric => {
-              return (
-                <tr key={metric.metric_id} className="border">
-                  <td>{metric.name}</td>
-                  <td>{metric.mean_control.toFixed(2)}</td>
-                  <td>{metric.mean_test.toFixed(2)}</td>
-                  <td>{metric.p_value.toFixed(4)}</td>
-                  <td>
-                    {metric.interval_start ? (
-                      <div>
-                        Chart: {(metric.interval_start * 100).toFixed(1)}% to {(metric.interval_end * 100).toFixed(1)}%
-                      </div>
-                    ) : (
-                      <p>No interval for binomial metrics</p>
-                    )}
-                  </td>
-                </tr>
-              );
-            })}
+            {metrics.map(metric => <MetricResultRow key={metric.metric_id} { ...metric } />)}
           </tbody>
         </table>
       )}
