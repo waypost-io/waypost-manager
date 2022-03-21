23c6cf697c3576c0f3b9c5dad7aafbf22b289b8f
Julia Martin <juliadmartin720@gmail.com>
2022-03-21 12:35:50 -0700

    Create page for flag events

---
+++ client/src/components/FlagLogPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FlagLogPage = () => {
+  return (
+    <div className="w-full p-12">
+      <h2 className="text-3xl font-bold text-primary-violet">Event Log</h2>
+    </div>
+  );
+};
+
+export default FlagLogPage;
