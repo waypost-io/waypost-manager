7af19c1b5916fd5de3137e7ce5b7c0767c32fb39
Julia Martin <juliadmartin720@gmail.com>
2022-03-21 13:37:22 -0700

    Implement filter by flag name

--- client/src/components/FlagLogPage.jsx
+++ client/src/components/FlagLogPage.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchLog } from '../actions/logActions';
+import { fetchLog } from "../actions/logActions";
 
 const LogItem = ({ id, flag_name, event_type, timestamp }) => {
   const eventDate = new Date(timestamp);
@@ -14,31 +14,63 @@ const LogItem = ({ id, flag_name, event_type, timestamp }) => {
   );
 };
 
-
 const FlagLogPage = () => {
-  const logItems = useSelector(state => state.flagLog);
-  console.log(logItems);
+  const logItems = useSelector((state) => state.flagLog);
+  const flagNames = logItems
+    .map((event) => event.flag_name)
+    .filter((elem, idx, arr) => arr.indexOf(elem) === idx);
+  const [filteredFlag, setFilteredFlag] = useState("all");
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchLog());
   }, [dispatch]);
 
+  const handleSelect = (e) => {
+    setFilteredFlag(e.target.value);
+  };
+
+  if (!logItems) return null;
+
   return (
     <div className="w-full p-12">
       <h2 className="text-3xl font-bold text-primary-violet">Event Log</h2>
+      <div>
+        <span className="mr-5">Filter for events by flag name:</span>
+        <select
+          onChange={handleSelect}
+          className="border border-slate rounded p-2 my-3"
+        >
+          <option value="all">All Flags</option>
+          {flagNames.map((name) => (
+            <option key={name} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
+      </div>
       <table className="w-full shadow-md mt-5">
-          <thead className="bg-primary-black text-primary-offwhite">
-            <tr>
-              <th>Flag Name</th>
-              <th>Event Type</th>
-              <th>Timestamp</th>
-            </tr>
-          </thead>
-          <tbody >
-            {logItems && logItems.map(event => <LogItem key={event.id} {...event} />)}
-          </tbody>
-        </table>
+        <thead className="bg-primary-black text-primary-offwhite">
+          <tr>
+            <th>Flag Name</th>
+            <th>Event Type</th>
+            <th>Timestamp</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logItems
+            .filter((event) => {
+              if (filteredFlag !== "all") {
+                return event.flag_name === filteredFlag;
+              } else {
+                return true;
+              }
+            })
+            .map((event) => (
+              <LogItem key={event.id} {...event} />
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 };
