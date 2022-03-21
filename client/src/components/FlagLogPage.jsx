998cef88446f88b0665c65be57e7f8f2f7730667
Julia Martin <juliadmartin720@gmail.com>
2022-03-21 13:04:40 -0700

    Display log as table

--- client/src/components/FlagLogPage.jsx
+++ client/src/components/FlagLogPage.jsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { fetchLog } from '../actions/logActions';
 
-const LogItem = () => {
+const LogItem = ({ id, flag_id, event_type, timestamp }) => {
   return (
-    <div className="flex justify-between p-5 border-b border-b-primary-oxfordblue">
-
-    </div>
+    <tr>
+      <td className="font-bold">{flag_id}</td>
+      <td>{event_type}</td>
+      <td>{timestamp}</td>
+    </tr>
   );
 };
 
@@ -23,7 +25,18 @@ const FlagLogPage = () => {
   return (
     <div className="w-full p-12">
       <h2 className="text-3xl font-bold text-primary-violet">Event Log</h2>
-      {logItems && logItems.map(event => <LogItem {...event} />)}
+      <table className="w-full shadow-md mt-5">
+          <thead className="bg-primary-black text-primary-offwhite">
+            <tr>
+              <th>Flag Name</th>
+              <th>Event Type</th>
+              <th>Timestamp</th>
+            </tr>
+          </thead>
+          <tbody >
+            {logItems && logItems.map(event => <LogItem key={event.id} {...event} />)}
+          </tbody>
+        </table>
     </div>
   );
 };
