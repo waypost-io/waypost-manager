7880f936fbe15db3dd456bcb91022b08bbc9df26
Julia Martin <juliadmartin720@gmail.com>
2022-03-21 14:00:05 -0700

    Remove console log

--- server/controllers/exposuresController.js
+++ server/controllers/exposuresController.js
@@ -9,7 +9,6 @@ const backfillData = async (req, res, next) => {
   try {
     const result = await experimentsTable.query("SELECT CURRENT_DATE - MIN(date_started) AS date_diff FROM experiments WHERE date_ended IS NULL");
     const dayDiff = result.rows[0]['date_diff'];
-    console.log(dayDiff);
     await backfillExposures(dayDiff);
     res.status(200).send("Successfully updated");
   } catch (err) {
