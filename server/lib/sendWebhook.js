db5a96629fe3210f6f1c94e4dc63e1125a644cf5
Caleb Smith <smithcaleb042@gmail.com>
2022-03-22 14:26:43 -0600

    added webhook-validator header to webhooks

--- server/lib/sendWebhook.js
+++ server/lib/sendWebhook.js
@@ -3,8 +3,9 @@ require("dotenv").config();
 
 async function sendWebhook(path, data) {
   const url = process.env.FLAG_PROVIDER_URL + path;
+  const headers = { "webhook-validator": process.env.WEBHOOK_VALIDATOR };
   try {
-    await axios.post(url, data);
+    await axios.post(url, data, { headers });
   } catch (err) {
     throw new Error(`Error sending webhook to ${path}`);
   }
