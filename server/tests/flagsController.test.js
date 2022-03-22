41097b1826880483b62ba0ce9bacfa588d1769fa
Julia Martin <juliadmartin720@gmail.com>
2022-03-22 12:54:13 -0700

    Set up skeleton code for flagsController test

---
+++ server/tests/flagsController.test.js
@@ -0,0 +1,29 @@
+const { getAllFlags, getFlag, createFlag, editFlag, deleteFlag } = require('../controllers/flagsController');
+
+describe('Test flag controller', () => {
+  const noop = () => {};
+
+  beforeEach(() => {});
+
+  afterEach(() => {});
+
+  test('getAllFlags returns all the flags', async () => {
+    // test getAllFlags
+  });
+
+  test('getFlag returns the flag by id', async () => {
+
+  });
+
+  test('createFlag creates a new flag and returns it', async () => {
+
+  });
+
+  test('editFlag updates a flag and returns updated flag', async () => {
+
+  });
+
+  test('deleteFlag sets the is_deleted field to true', async () => {
+
+  });
+});
